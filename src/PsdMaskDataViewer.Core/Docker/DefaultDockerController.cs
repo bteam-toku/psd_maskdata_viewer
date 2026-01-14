87b9cec9651b6b6c326e841728330888c186dc3b
ryuta.tokunaga <bteam.toku@gmail.com>
2026-01-14 19:58:08 +0900

    fix:docker runのパラメータを修正（パラメータ間の干渉回避）

--- src/PsdMaskDataViewer.Core/Docker/DefaultDockerController.cs
+++ src/PsdMaskDataViewer.Core/Docker/DefaultDockerController.cs
@@ -19,11 +19,15 @@ namespace PsdMaskDataViewer.Core.Docker
         /// <returns>docker runコマンドを含む文字列。この文字列をShellで実行してください。</returns>
         public override string BuildRunCommand(string inputPath, string outputPath)
         {
+            // カレントパスを取得
+            var currentPath = Environment.CurrentDirectory;
+            // docker runコマンドを構築
             var sb = new System.Text.StringBuilder();
             sb.Append("docker run --rm ");
             sb.Append($"-v '{inputPath}:/app/input' ");
             sb.Append($"-v '{outputPath}:/app/output' ");
-            sb.Append($"{ImageName} --input /app/input --output /app/output");
+            sb.Append($"-v '{currentPath}:/data' ");
+            sb.Append($"{ImageName}:latest --input /app/input --output /app/output");
             return sb.ToString();
         }
     }
